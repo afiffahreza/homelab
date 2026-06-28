57789f846eff54a7315878177dff78fd7758ceac
afiffahreza <afiffahreza3@gmail.com>
2026-06-27 22:12:08 -0400

    chore: move tfstate to R2

--- terraform/cloudflare/mew/main.tf
+++ terraform/cloudflare/mew/main.tf
@@ -1,4 +1,22 @@
 terraform {
+  backend "s3" {
+    bucket = "homelab-tfstate"
+    key    = "cloudflare/mew/terraform.tfstate"
+    region = "auto"
+    
+    endpoints = {
+      s3 = "https://f0a809b662083371772d347d2acdad2a.r2.cloudflarestorage.com"
+    }
+
+    # Enable OpenTofu native S3 locking
+    use_lockfile = true 
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
+  }
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
